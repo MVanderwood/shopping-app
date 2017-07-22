2ed556474d8327adbafb8f25e07d5e4cebf284fa
MVanderwood <m.b.vanderwood@gmail.com>
2017-07-22 13:19:01 -0500

    fixed up Cart component (visual only)

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,11 +8,37 @@ class Cart extends Component {
     }
 
     render() {
-        const cart = this.props.cart
+        const cart = this.props.cart;
         return (
             <div>
-                <h3>Order Summary</h3>
-                <p>Items Ordered: {cart.length}</p>
+                <div className="cart-header">
+                    <h3>Order Summary</h3>
+                    <p>Items Ordered: {cart.length}</p>
+                </div>
+                <table className="cart-summary">
+                    <tbody>
+                        <tr className="summary-item">
+                            <td>Items:</td>
+                            <td>${5.00}</td>
+                        </tr>
+                        <tr className="summary-item">
+                            <td>Shipping & handling:</td>
+                            <td>${5.00}</td>
+                        </tr>
+                        <tr className="summary-item">
+                            <td>Total before tax:</td>
+                            <td>${5.00}</td>
+                        </tr>
+                        <tr className="summary-item">
+                            <td>Estimated tax:</td>
+                            <td>${5.00}</td>
+                        </tr>
+                        <tr className="summary-total">
+                            <td>Order Total:</td>
+                            <td>${5.00}</td>
+                        </tr>
+                    </tbody>
+                </table>
                 <button>Review My Order</button>
             </div>
         );
